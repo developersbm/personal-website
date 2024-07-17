e609f1172b82a4e26c606a6fb61a1c37981f64b9
developersbm <sbastidamarin@student.cccd.edu>
2024-07-17 13:11:16 +0200

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/preline/preline.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("preline/plugin")],
+};
